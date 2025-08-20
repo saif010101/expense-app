de508a0ec3bfc2f99c63e8010414c6bf8e77abe6
Muhammad Saif <saif01100110@gmail.com>
2025-08-20 05:40:13 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,122 @@
+import { useState } from "react";
+import { useRef } from "react";
+import "./index.css";
+
+function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <>
+      <main className="m-5 flex flex-col gap-3">
+        <h1 className="text-2xl font-bold">Fasih's Dashboard</h1>
+        <div className="p-4 bg-white border-1 border-gray-300 shadow-md rounded-lg">
+          <span className="font-[600]">
+            Net Balance: <span className="ml-2">-150 PKR</span>
+          </span>
+        </div>
+        {/* To Pay Card */}
+        <div className="flex flex-col gap-2 p-4 bg-white border-1 border-gray-300 shadow-md rounded-lg">
+          <span className="font-bold text-xl">To Pay</span>
+          <div className="flex justify-between items-center">
+            <span className="font-[500]">Ali - </span>
+            <span className="mr-auto ml-2">Rs. 200</span>
+            <button className="text-white text-sm bg-blue-500 hover:bg-blue-400 cursor-pointer px-2 py-1 rounded-lg">
+              Clear
+            </button>
+          </div>
+          <div className="flex justify-between items-center">
+            <span className="font-[500]">Hamza - </span>
+            <span className="mr-auto ml-2">Rs. 100</span>
+            <button className="text-white text-sm bg-blue-500 hover:bg-blue-400 cursor-pointer px-2 py-1 rounded-lg">
+              Clear
+            </button>
+          </div>
+        </div>
+        {/* To Recieve Card */}
+        <div className="flex flex-col gap-2 p-4 bg-white border-1 border-gray-300 shadow-md rounded-lg">
+          <span className="font-bold text-xl">To Receive</span>
+          <div className="flex justify-between items-center">
+            <span className="font-[500]">Ali - </span>
+            <span className="mr-auto ml-2">Rs. 200</span>
+            <button className="text-white text-sm bg-blue-500 hover:bg-blue-400 cursor-pointer px-2 py-1 rounded-lg">
+              Clear
+            </button>
+          </div>
+          <div className="flex justify-between items-center">
+            <span className="font-[500]">Hamza - </span>
+            <span className="mr-auto ml-2">Rs. 100</span>
+            <button className="text-white text-sm bg-blue-500 hover:bg-blue-400 cursor-pointer px-2 py-1 rounded-lg">
+              Clear
+            </button>
+          </div>
+        </div>
+      </main>
+      {/* Dialog */}
+      {isModalOpen && (
+        <div className="absolute top-0 w-full h-screen flex justify-center items-center bg-black/30 ">
+          <div className="p-4 bg-white flex flex-col gap-2 rounded-lg border-1 border-gray-400">
+            <label className="font-[600]" htmlFor="paidBy">
+              Paid by
+            </label>
+            <select
+              className="border-1 border-gray-400 px-2 py-1 rounded-md"
+              name=""
+              id="paidBy"
+            >
+              <option value="Ali">Ali</option>
+              <option value="Hamza">Hamza</option>
+              <option value="Noor">Noor</option>
+            </select>
+            <label className="font-[600]" htmlFor="totalCost">
+              Total cost
+            </label>
+            <input
+              className="px-2 py-1 border-1 border-gray-400 rounded-md"
+              type="text"
+              name=""
+              id=""
+            />
+            <div className="">
+              <span>Participated</span>
+              <div>
+                <input type="checkbox" name="" id="saif" />
+                <label className="ml-2 font-[500]" htmlFor="saif">
+                  Saif
+                </label>
+              </div>
+              <div>
+                <input type="checkbox" name="" id="fasih" />
+                <label className="ml-2 font-[500]" htmlFor="fasih">
+                  Fasih
+                </label>
+              </div>
+              <div>
+                <input type="checkbox" name="" id="yaqub" />
+                <label className="ml-2 font-[500]" htmlFor="yaqub">
+                  Yaqub
+                </label>
+              </div>
+              <div>
+                <input type="checkbox" name="" id="mahad" />
+                <label className="ml-2 font-[500]" htmlFor="mahad">
+                  Mahad
+                </label>
+              </div>
+              <div>
+                <input type="checkbox" name="" id="atif" />
+                <label className="ml-2 font-[500]" htmlFor="atif">
+                  Atif
+                </label>
+              </div>
+            </div>
+            <button className="py-1 bg-blue-500 hover:bg-blue-400 rounded-md text-white">
+              Add Meal
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
